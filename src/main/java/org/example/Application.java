78bbf148281d5bcc75c748de6355a3217f0b4e4f
BuildTools <unconfigured@null.spigotmc.org>
2023-05-18 01:24:26 +0900

    さるののぱーふぇくとちんすうきょうしつ

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -12,16 +12,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 import java.util.Map;
 
 @SpringBootApplication
 @EnableAsync
-public class Application {
+public class Application implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new RateLimitInterceptor());
     }
 }
 
---
+++ src/main/java/org/example/RateLimitInterceptor.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import io.github.bucket4j.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.time.Duration;
+
+public class RateLimitInterceptor implements HandlerInterceptor {
+
+    private final Bucket bucket;
+
+    public RateLimitInterceptor() {
+        // ここでレート制限のポリシーを定義します。この例では、60秒あたり100リクエストまでとしています。
+        Bandwidth limit = Bandwidth.simple(3, Duration.ofMinutes(5));
+        this.bucket = Bucket4j.builder().addLimit(limit).build();
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        // レート制限に達した場合はリクエストを拒否します。
+        if (bucket.tryConsume(1)) {
+            return true;
+        } else {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value()); // 429 status
+            return false;
+        }
+    }
+}
